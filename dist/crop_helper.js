da0c8d77fd9f4c3326599195830862bdccbe9ee8
Anders Wood <anders.wood@brandfolder.com>
2019-08-12 11:53:19 -0600

    delete dist dir

--- dist/crop_helper.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.availableOptions = void 0;
-var availableOptions = ['viewMode', 'dragMode', 'initialAspectRatio', 'aspectRatio', 'data', 'preview', 'responsive', 'restore', 'checkCrossOrigin', 'checkOrientation', 'modal', 'guides', 'center', 'highlight', 'background', 'autoCrop', 'autoCropArea', 'movable', 'rotatable', 'scalable', 'zoomable', 'zoomOnTouch', 'zoomOnWheel', 'wheelZoomRatio', 'cropBoxMovable', 'cropBoxResizable', 'toggleDragModeOnDblclick', 'minContainerWidth', 'minContainerHeight', 'minCanvasWidth', 'minCanvasHeight', 'minCropBoxWidth', 'minCropBoxHeight', // event shortcut functions
-'ready', 'cropstart', 'cropmove', 'crop', 'zoom'];
-exports.availableOptions = availableOptions;
